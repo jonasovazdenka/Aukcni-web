854fcf1fd8ece3e362664a4455e4963d4dd1cd22
Zdenka Jonasova <86010709+zdenkajonasov@users.noreply.github.com>
2022-01-17 15:13:43 +0200

    Update script.js

--- p5/script.js
+++ p5/script.js
@@ -1,13 +1,23 @@
 var ball = { 
     x: 0, 
     y: 0,
-    r: 40,
+    r: 100,
     direction: p5.Vector.random2D(),
     speed: 5,
     move: function () {
-        this.x += this.direction.x * this.speed;
-        this.y += this.direction.y * this.speed;
+        let newX = this.x + this.direction.x * this.speed;
+        let newY = this.y + this.direction.y * this.speed;
+        if (newX + this.r < 0 || newX > innerWidth) {
+            this.direction.x *= -1;
+
+        }
+        if (newY + this.r < 0 || newY > innerHeight) {
+            this.direction.y *= -1;
+        }
+        this.y = newY;
+        this.x = newX;
     },
+    
     draw: function () {
         ellipse(ball.x, ball.y, 2*ball.r, 2*ball.r);
     }
