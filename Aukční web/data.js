65c37562e129c1d7aa17a4af843648cba5ed8f31
Zdenka Jonasova <86010709+zdenkajonasov@users.noreply.github.com>
2022-03-21 12:57:20 +0100

    Add files via upload

---
+++ Aukční web/data.js
@@ -0,0 +1,21 @@
+const data = {
+        p001: {
+        name: "Na volné noze",
+        desc: "Popis",
+        author: "Jan Melvil Publishing",
+        images: "img/Na_vole_noze.png",
+        },
+        p002: {
+            name: "Šum",
+            desc: "",
+            author: "",
+            images: "",
+            
+        },
+        p003: {
+            name: "ne",
+            desc: "",
+            author: "",
+            images: "",
+        },
+    }
---
+++ Aukční web/script.js
@@ -0,0 +1,10 @@
+const gallery = document.getElementById("gallery");
+for (let index in data){
+    console.log(index);
+    let item = data[index];
+    let ele = document.createElement("div");
+    ele.innerHTML = '<h2>${item.name}</h2> <p>${item.desc}</p> <p>${item.author}</p>';
+    let p = document.createElement("p");
+    p.innerText = item.name;
+    gallery.appendChild(p);
+}
