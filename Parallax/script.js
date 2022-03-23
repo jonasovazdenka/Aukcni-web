e67b95209e42611c5a57b48bda50524b9ff41bfc
Zdenka Jonasova <86010709+zdenkajonasov@users.noreply.github.com>
2022-03-23 10:01:16 +0100

    Delete Parallax directory

--- Parallax/script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = () => {
-    console.log(window.scrollY);
-    document.getElementById("L1").style.top = window.scrollY * 0.8 + "px";
-    document.getElementById("L2").style.top = window.scrollY * 0.5 + "px";
-    document.getElementById("L3").style.top = window.scrollY * 0.3 + "px";
-    
-};
