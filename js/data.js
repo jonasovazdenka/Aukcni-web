e656e45b4f61cb0226a24b9576e3eb2a37a6f729
Zdenka Jonasova <zdenka.jonasova@post.cz>
2022-03-23 17:19:59 +0100

    update

--- js/data.js
+++ js/data.js
@@ -1,21 +1,20 @@
 const data = {
-        p001: {
-        name: "Na volné noze",
-        desc: "Popis",
-        author: "Robert Vlach",
-        images: ["Na_vole_noze.png"],
-        },
-        p002: {
-            name: "Šum",
-            desc: "Popis",
-            author: "Cass R. Sunstein, Daniel Kahneman, Olivier Sibony",
-            images: "",
-            
-        },
-        p003: {
-            name: "Tělo sčítá rány",
-            desc: "Jak trauma dopadá na naši mysl i zdraví a jak se z něj léčit",
-            author: "Bessel Van der Kolk",
-            images: "",
-        },
-    }
+  p001: {
+    name: "Na volné noze",
+    desc: "Popis",
+    author: "Robert Vlach",
+    images: "/p001.png",
+  },
+  p002: {
+    name: "Šum",
+    desc: "Popis",
+    author: "Cass R. Sunstein, Daniel Kahneman, Olivier Sibony",
+    images: ["./sum.png"],
+  },
+  p003: {
+    name: "Tělo sčítá rány",
+    desc: "Jak trauma dopadá na naši mysl i zdraví a jak se z něj léčit",
+    author: "Bessel Van der Kolk",
+    images: ["telo-scita-rany.png"],
+  },
+};
--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-console.log('hello');
 const galleryElem = document.getElementById("gallery");
-const detailElem = document.getElementById|("detail");
-for (let index in data){
-    let itemData = data[index];
-    let itemElement = document.createElement("div");
-    itemElement.innerHTML = `<figure><img src="./data/${itemData.images[0]}"></figure>
+const detailElem = document.getElementById | "detail";
+for (let index in data) {
+  let itemData = data[index];
+  let itemElement = document.createElement("div");
+  itemElement.innerHTML = `<figure><img src="./data/${itemData.images[0]}"></figure>
     <div><h2>${itemData.name}</h2>
     <p class="desc">${itemData.desc}</p>
     <p class="author">${itemData.author}</p></div>`;
-    galleryElem.appendChild(itemElement);
-    itemElement.onclick = () => {
-        detailElem.innerHTML = `<div class ="detail-item">
+  galleryElem.appendChild(itemElement);
+  itemElement.onclick = () => {
+    detailElem.innerHTML = `<div class ="detail-item">
         <figure><img src="./data/${itemData.images[0]}></figure>
         <div><h2>${itemData.name}</h2>
         <p class="desc">${itemData.desc}</p>
         <p class="author">${itemData.author}</p></div>
         </div>`;
-            detailElem.style.display = "flex";
-    };
-    detailElem.onclick = () => {
-        detailElem.style.display = "none";
-    };
+    detailElem.style.display = "flex";
+  };
+  detailElem.onclick = () => {
+    detailElem.style.display = "none";
+  };
 }
