c616936b0e30d711e794d11d3a0069bb4ab15b48
Zdenka Jonasova <zdenka.jonasova@post.cz>
2022-03-23 16:19:44 +0100

    upgrade

--- Aukční web/script.js
+++
@@ -1,9 +0,0 @@
-const gallery = document.getElementById("gallery");
-for (let index in data){
-    let item = data[index];
-    let ele = document.createElement("div");
-    ele.innerHTML = `<h2>${item.name}</h2> <p>${item.desc}</p> <p>${item.author}</p>`;
-    let p = document.createElement("p");
-    p.innerText = item.name;
-    gallery.appendChild(ele);
-}
---
+++ aukcni-web/js/data.js
@@ -0,0 +1,21 @@
+const data = {
+        p001: {
+        name: "Na volné noze",
+        desc: "Popis",
+        author: "Robert Vlach",
+        images: ["Na_vole_noze.png"],
+        },
+        p002: {
+            name: "Šum",
+            desc: "Popis",
+            author: "Cass R. Sunstein, Daniel Kahneman, Olivier Sibony",
+            images: "",
+            
+        },
+        p003: {
+            name: "Tělo sčítá rány",
+            desc: "Jak trauma dopadá na naši mysl i zdraví a jak se z něj léčit",
+            author: "Bessel Van der Kolk",
+            images: "",
+        },
+    }
