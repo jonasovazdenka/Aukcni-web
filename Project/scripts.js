0d2f8bac42143dfa88bd0262cda6decf75691a1d
Zdenka Jonasova <86010709+zdenkajonasov@users.noreply.github.com>
2022-03-23 10:01:23 +0100

    Delete Project directory

--- Project/scripts.js
+++
@@ -1,5 +0,0 @@
-const getValueInput = () =>{
-    let inputValue = document.getElementById("domTextElement").value; 
-    document.getElementById("domTextElement").innerHTML = inputValue;
-    console.log(document.getElementById('domTextElement').value); 
-  }
